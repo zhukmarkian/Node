fe9aa4982b75391b2df1cb11d775e066f582b17f
zhukmarkian <zhukmarkian.t@gmail.com>
2021-03-03 16:59:21 +0200

    aaa

--- node/lesson4/service/user.service.js
+++ node/lesson4/service/user.service.js
@@ -1,40 +1,27 @@
-const User=require('../dataBase/models/User')
-const { promisify } = require('util');
-const fs = require('fs');
-const readFile = promisify(fs.readFile);
-const writeFile = promisify(fs.writeFile);
-module.exports={
-    findUsers: ()=>{
-        return User.find
+const User = require('../dataBase/models/User');
+
+module.exports = {
+    findUsers: () => User.find,
+
+    findUsersById: (userId) => {
+        (
+            User.findById(userId)
+        );
     },
 
-    findUsersById:  (userId) => {
-        User.findById(userId)
-    },
-    createUser: (userObject)=>{
-         User.create({...userObject,gender:'male'});
+    createUser: async (userObject) => {
+        await User.create({ ...userObject, gender: 'male' });
     },
 
-    findUsersByEmail:(email ) => {
-
-
-
-         User.find(user => user.email === email);
-    },
-    findUsersByName:(name ) => {
-
-
-
-       User.find(user => user.name === name);
+    findUsersByEmail: (email) => {
+        User.find((user) => user.email === email);
     },
 
+    findUsersByName: (name) => {
+        User.find((user) => user.name === name);
+    },
     deleteUser: async (userId) => {
-
-
-        User.splice(userId, 1);
-
-        User.writeFile( User);
+        await User.splice(userId, 1);
+        User.writeFile(User);
     },
-
 };
-
